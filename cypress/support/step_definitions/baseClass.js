bb0df40f27246d53fe4f1c3d6818f0c02a0b1e41
therohit12 <rkchoudhary773@gmail.com>
2024-12-16 22:41:19 -0800

    Code Updated

--- cypress/support/step_definitions/baseClass.js
+++
@@ -1,6 +0,0 @@
-import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
-/// <reference types="Cypress" />
-
-Given("I am opening a URL", () => {
-    cy.visit("https://www.saucedemo.com/")
-})
